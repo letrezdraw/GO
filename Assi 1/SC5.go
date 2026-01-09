b0aa0f02c9a36050dfbfcb16237e85ffec99ed05
REZ <147749303+letrezdraw@users.noreply.github.com>
2026-01-09 10:59:31 +0000

    g

--- Assi 1/SC5.go
+++ Assi 1/SC5.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	var str1, str2 string
-	fmt.Print("Enter first string: ")
-	fmt.Scanln(&str1)
-	fmt.Print("Enter second string: ")
-	fmt.Scanln(&str2)
+	reader := bufio.NewReader(os.Stdin)
 
-	if strings.Contains(str2, str1) {
-		fmt.Println(str1, "is a substring of", str2)
+	fmt.Print("Enter first string: ")
+	str1, _ := reader.ReadString('\n')
+	str1 = strings.TrimSpace(str1)
+
+	fmt.Print("Enter second string: ")
+	str2, _ := reader.ReadString('\n')
+	str2 = strings.TrimSpace(str2)
+
+	if strings.Contains(str1, str2) {
+		fmt.Println(str2, "is a substring of", str1)
 	} else {
-		fmt.Println(str1, "is not a substring of", str2)
+		fmt.Println(str2, "is not a substring of", str1)
 	}
 }
